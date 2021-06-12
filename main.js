91549bd92fecdae2ebc949bef9e71e295d90d7a9
Omen <omen@localhost.localdomain>
2021-06-12 20:11:26 +0100

    minimal complete 3js with animation

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 import './style.css'
 import * as THREE  from 'three';
 
+let scene, camera, renderer, skyboxGeo, skybox;
 
 
+function init(){
+  scene = new THREE.Scene();
 
-//scene
-const scene = new THREE.Scene();
+  //camera
+  camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,2,200);
+  camera.position.setZ(50);
+  //renderer
+  renderer = new THREE.WebGL1Renderer({
+    canvas:document.querySelector('#bg'),
+  });
 
-//camera
-const camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,2,200);
-camera.position.setZ(50);
-//renderer
-const renderer = new THREE.WebGL1Renderer({
-  canvas:document.querySelector('#bg'),
-});
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth,window.innerHeight);
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.render(scene, camera);
+  animate();
+}
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
 
-renderer.render(scene, camera);
+init();
