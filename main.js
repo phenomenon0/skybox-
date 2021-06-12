810dec9014f08e768915da619b30954c44b4d176
Omen <omen@localhost.localdomain>
2021-06-12 20:06:30 +0100

    minimal state js

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
 import './style.css'
+import * as THREE  from 'three';
 
-import * as  THREE from './node_modules/three/build/three.js';
 
 
-const scene,camera, renderer ;
 
-scene = new THREE.Scene();
-renderer = new THREE.WebGL1Renderer({canvas:document.querySelector('#bg')});
+//scene
+const scene = new THREE.Scene();
+
+//camera
+const camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,2,200);
+camera.position.setZ(50);
+//renderer
+const renderer = new THREE.WebGL1Renderer({
+  canvas:document.querySelector('#bg'),
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
 
-camera  = new THREE.PerspectiveCamera(75,window.innerHeight/window.innerWidth,1,1000)
-camera.position.setZ(10);
-renderer.render(scene,camera)
-
-const geometry = new THREE.TorusGeometry(20,6,20,40, Math.PI*1.8) 
-const material = new THREE.MeshStandardMaterial({color:0xffffff, wireframe:true});
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus)
-function animate(){
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-
-
-}
-
-animate()
+renderer.render(scene, camera);
